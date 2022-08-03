6b97ffa0c95e734b4914be0851702a4bb555f2d5
Mateus Brasil <Mateus-code-98>
2022-08-03 09:10:07 -0300

    Preparando revisão de código

--- src/schemas/authSchema.ts
+++
@@ -1,9 +0,0 @@
-import { ICreateUserData } from './../services/authService';
-import Joi from "joi";
-
-const authSchema = Joi.object<ICreateUserData>({
-    email: Joi.string().email({tlds: {allow: false}}).required(),
-    password: Joi.string().required()
-});
-
-export default authSchema;
--- src/schemas/notesSchema.ts
+++
@@ -1,9 +0,0 @@
-import { ICreateNotesData } from './../services/notesService';
-import Joi from "joi";
-
-const notesSchema = Joi.object<ICreateNotesData>({
-    title: Joi.string().max(50).required(),
-    annotation: Joi.string().max(1000).required()
-});
-
-export default notesSchema;
